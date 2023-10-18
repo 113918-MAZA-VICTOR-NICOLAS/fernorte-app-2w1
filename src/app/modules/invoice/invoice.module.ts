267392bdc24a7f032b4f6abd09e74857ae800af4
nicomaza <nicolasmaza66@hotmail.com>
2023-10-18 16:04:06 -0300

    primer commit
    detalle de facturas

--- src/app/modules/invoice/invoice.module.ts
+++ src/app/modules/invoice/invoice.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
+import { InvoiceDetailComponent } from './components/invoice-detail/invoice-detail.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, InvoiceDetailComponent],
   providers: [],
   imports: [CommonModule],
   exports: [HomeComponent],
